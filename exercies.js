63536b6a0ab3e5063830fed700730f3750403c95
ivanhack34 <48842215+ivanhack34@users.noreply.github.com>
2023-01-16 11:40:49 -0400

    Exercise of day 17 part 2: 16/1/2023
    Complete los ejercicios 1 y 2 de este modulo

---
+++ exercies.js
@@ -0,0 +1,21 @@
+//TODO: Exercise of day 17 part 2: 16/1/2023
+
+//?1-Use an Array to Store a Collection of Data
+
+/*
+let yourArray = ["a", 2, true, "c", null, { name: "john" }];
+console.log(yourArray);
+*/
+
+//?2-Access an Array's Contents Using Bracket Notation
+
+/*
+let myArray = ["a", "b", "c", "d"];
+// Only change code below this line
+myArray[1] = "Is this b"
+// Only change code above this line
+console.log(myArray);
+*/
+
+//?3-Add Items to an Array with push() and unshift()
+
